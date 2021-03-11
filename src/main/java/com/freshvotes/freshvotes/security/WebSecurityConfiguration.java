7067fc44afb7badbcc175708bc60e4224cc53031
Saiffer <aznaurovdato@gmail.com>
2021-03-10 22:55:42 -0500

    Login page and Login Controller

--- src/main/java/com/freshvotes/freshvotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/freshvotes/freshvotes/security/WebSecurityConfiguration.java
@@ -20,15 +20,16 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
-				.authorizeRequests().antMatchers("/").permitAll()
-				.anyRequest().hasRole("USER").and()
+				.authorizeRequests()
+				  .antMatchers("/").permitAll()
+				  .anyRequest().hasRole("USER").and()
 				.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.and()
+				  .loginPage("/login")
+				  .permitAll()
+				  .and()
 				.logout()
-				.logoutUrl("/logout")
-				.permitAll();
+				  .logoutUrl("/logout")
+				  .permitAll();
 		
 	}
 	
