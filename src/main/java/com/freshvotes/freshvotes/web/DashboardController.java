98361c346960898918fdca41600fecce6ed641e1
Saiffer <aznaurovdato@gmail.com>
2021-03-10 23:19:44 -0500

    Login logout testing functionality

--- src/main/java/com/freshvotes/freshvotes/web/DashboardController.java
+++ src/main/java/com/freshvotes/freshvotes/web/DashboardController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
-
+	
 	@GetMapping("/")
 	public String rootView() {
 		return "index";
+	}
+	
+	@GetMapping("/dashboard") 
+	public String dashboard() {
+		return "dashboard";
 	}
 	
 }
